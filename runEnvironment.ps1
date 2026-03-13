070e5e8bbcf990dd9bf04c68481c43131a675c9d
Soufiane Bouaddis <111077308+soufianebouaddis@users.noreply.github.com>
2026-03-13 11:12:26 +0000

    update  powershell script

--- runEnvironment.ps1
+++ runEnvironment.ps1
@@ -1,17 +1,17 @@
 # 1. Start Redis
 Write-Host "Starting Redis..." -ForegroundColor Cyan
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd 'C:\Users\cdm-u9160\Desktop\Java\Redis-x64-5.0.14.1'; .\redis-server.exe"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd 'C:\Java\Redis-x64-5.0.14.1'; .\redis-server.exe"
 
 # 2. Start PostgreSQL
 Write-Host "Starting PostgreSQL..." -ForegroundColor Elephant
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd 'C:\Users\cdm-u9160\Desktop\Java\pgsql\bin'; .\postgres.exe -D 'C:\Users\cdm-u9160\Desktop\Java\pgsql\data'"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd 'C:\Java\pgsql\bin'; .\postgres.exe -D 'C:\Java\pgsql\data'"
 
 # 3. Start Kafka (using Java 21 environment)
 Write-Host "Starting Kafka..." -ForegroundColor Yellow
 
 $KafkaCommand = @"
 # Set JAVA_HOME
-`$env:JAVA_HOME = 'C:\Users\cdm-u9160\Downloads\jdk-21.0.5_windows-x64_bin\jdk-21.0.5'
+`$env:JAVA_HOME = 'C:\Downloads\jdk-21.0.5_windows-x64_bin\jdk-21.0.5'
 
 # Update Path
 `$env:Path = `$env:JAVA_HOME + '\bin;' + `$env:Path
